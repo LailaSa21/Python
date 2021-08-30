370f4eedb9a9c26f4b042a60ff72f8bf28f96965
LailaSa21 <87502888+LailaSa21@users.noreply.github.com>
2021-08-29 21:42:09 -0500

    first commit

---
+++ phone-d.py
@@ -0,0 +1,31 @@
+Names = ['Amal', 'Mohammed', 'Khadijah',
+         'Abdullah', 'Rawan', 'Faisal', 'Layla']
+Phones = [1111111111, 2222222222, 3333333333,
+          4444444444, 5555555555, 6666666666, 7777777777]
+Phone = 1111111111
+if Phone == 1111111111:
+    print("You are ", Names[0])
+elif Phone == 2222222222:
+    print('You are ', Names[1])
+elif Phone == 3333333333:
+    print('You are ', Names[2])
+elif Phone == 4444444444:
+    print('You are ', Names[3])
+elif Phone == 5555555555:
+    print('You are ', Names[4])
+elif Phone == 6666666666:
+    print('You are ', Names[5])
+elif Phone == 7777777777:
+    print('You are ', Names[6])
+
+elif Phone != [Phone]:
+    print('Sorry, the number is not found ')
+else:
+    print('This is invalid number')
+
+
+# Add names to list Or Add Phone to list
+# Names.append('Laila')
+# print(Names)
+# Phone.append(9999999999)
+# print(Phone)
